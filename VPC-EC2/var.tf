64711e147c2428393329a3008a00725ea268e0c9
AhmedYousif22 <ahmedyousif9422@outlook.com>
2020-05-29 01:20:28 +0400

    recomit

---
+++ VPC-EC2/var.tf
@@ -0,0 +1,29 @@
+
+# AWS ACCESS AND REGION
+variable "AWS_ACCESS_KEY" {}
+
+variable "AWS_SECRET_KEY" {}
+
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+
+
+# AWS AMIs
+
+variable "AMIs" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-03e33c1cefd1d3d74"
+    us-west-1 = "ami-0ee1a20d6b0c6a347"
+    eu-west-1 = "ami-0a4fbfab82b4aee91"
+  }
+}
+
+
+
+
+
+
+
+
